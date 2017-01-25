023a852debe0f9aad622f852aea0e0e6d9e8e3ce
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-25 03:28:32 +0000

    part 2

--- Databases/cats.js
+++ Databases/cats.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/cat_app");
 
 var catSchema = new mongoose.Schema({
@@ -8,3 +9,44 @@ var catSchema = new mongoose.Schema({
 });
 
 var Cat = mongoose.model("Cat", catSchema);
+
+//adding a new cat to the DB
+
+// var george = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7,
+//     temperament: "Evil"
+// });
+
+// george.save(function(err, cat) {
+//     if (err) {
+//         console.log("Something went wrong");
+//     } else {
+//         console.log("We just saved a cat to the DB:");
+//         console.log(cat);
+//     }
+// });
+
+Cat.create({
+    name: "Snow White",
+    age: 15,
+    temperament: "Bland"
+}, function(err, cat) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(cat);
+    }
+});
+
+//retrieve all cats from the DB and console.log each one
+
+Cat.find({}, function(err, cats) {
+    if (err) {
+        console.log("Oh no, error!");
+        console.log(err);
+    } else {
+        console.log("All the cats...");
+        console.log(cats);
+    }
+});
